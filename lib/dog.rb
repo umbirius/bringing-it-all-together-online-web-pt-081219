011cfe12ab82a047675b216306466d8a69406cb9
Umberto Palazzo <upalazzo93@gmail.com>
2019-10-21 19:08:43 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,7 +2,7 @@ class Dog
   
   attr_accessor :id, :name, :breed 
   
-  def initialize(name:, breed:, :id)
+  def initialize(name:, breed:, id: nil)
     @name = name
     @breed = breed 
     @id = id
