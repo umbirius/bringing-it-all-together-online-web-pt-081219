a1477e91f270632fff48fd9fdf6d170dce124471
Umberto Palazzo <upalazzo93@gmail.com>
2019-10-21 19:13:40 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -24,10 +24,18 @@ class Dog
     DB[:conn].execute(sql)
   end 
   
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    breed = row[2]
+    self.new(id: id, name: name, breed: breed)
+  end 
+  
   def self.find_by_name(name)
-    sql = "SELECT * FROM dogs WHERE name = ?"
-    result = DB[:conn].execute(sql, name)[0]
-    Dog.new(result[0], result[1], result[2])
+    sql = "SELECT * FROM dogs WHERE name = ? LIMIT 1"
+    result = DB[:conn].execute(sql, name) do |row|
+      self.new_from_db(row)
+    end.first 
   end 
   
   def self.create(name, breed)
