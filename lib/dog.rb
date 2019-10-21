838e51a085be8c2f947226c7d4f33f9212591a03
Umberto Palazzo <upalazzo93@gmail.com>
2019-10-21 19:14:04 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -33,7 +33,7 @@ class Dog
   
   def self.find_by_name(name)
     sql = "SELECT * FROM dogs WHERE name = ? LIMIT 1"
-    result = DB[:conn].execute(sql, name) do |row|
+    result = DB[:conn].execute(sql, name).map do |row|
       self.new_from_db(row)
     end.first 
   end 
